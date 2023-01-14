988c3d09a683cfe40782f6445e51b3534235309e
Taras Byalyk <byalykt@hotmail.com>
2023-01-14 23:07:00 +0100

    Code was run through Beautifier

--- feedback.php
+++ feedback.php
@@ -1,33 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Feedback</title>
-  <link rel="stylesheet" href="css/style.css" />
-</head>
-<body>
-  <?php
-$name = $_POST['name'];
-$email = $_POST['email'];
-$comment = $_POST['comment'];
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Feedback</title>
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+  <body>
+<?php
+$name = $_POST["name"];
+$email = $_POST["email"];
+$comment = $_POST["comment"];
 
-$conn = new mysqli('localhost', 'root', '', 'project');
+$conn = new mysqli("localhost", "root", "", "project");
 if ($conn->connect_error) {
-  die('Connection failed: ' . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 } else {
-  $stmt = $conn->prepare("INSERT INTO feedback(name, email, comment) VALUES(?,?,?)");
-  $stmt->bind_param('sss', $name, $email, $comment);
-  $stmt->execute();
-  echo "Your comment was successfully sent!";
-  $stmt->close();
-  $conn->close();
+    $stmt = $conn->prepare(
+        "INSERT INTO feedback(name, email, comment) VALUES(?,?,?)"
+    );
+    $stmt->bind_param("sss", $name, $email, $comment);
+    $stmt->execute();
+    echo "Your comment was successfully sent!";
+    $stmt->close();
+    $conn->close();
 }
 ?>
-<nav><ul class="menu"><li><a href="index.html">Go home</a></li></ul></nav>
-
-</body>
+<nav>
+      <ul class="menu">
+        <li>
+          <a href="index.html">Go home</a>
+        </li>
+      </ul>
+    </nav>
+  </body>
 </html>
 
 
